d6d701e32612be9dd94afadd742d593fcb5efd9c
jailsonn <jailsonnogueira2@gmail.com>
2022-05-16 21:39:51 -0300

    projeto iniciado

---
+++ src/main/java/com/jailsonn/superior/userdept/UserdeptApplication.java
@@ -0,0 +1,13 @@
+package com.jailsonn.superior.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdeptApplication.class, args);
+	}
+
+}
